ab0b8ce9a63a3fc6dae7797b8cfaa7cd2814ca0a
mrpythen <herman.nysted@gmail.com>
2024-02-18 17:46:29 +0100

    Laget script for validering av input felter + oppdatert index

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,5 +1,7 @@
+//array for å lagre bilettobjekter
 let biletter = [];
 function bestillBiletter(){
+    //henter verdier fra inputfeltene
     let filmer = document.getElementById("fimer").value;
     let antall = document.getElementById("antall").value;
     let fornavn = document.getElementById("fornavn").value;
@@ -7,5 +9,46 @@ function bestillBiletter(){
     let telefonnr = document.getElementById("telefonnr").value;
     let epost = document.getElementById("epost").value;
 
+    //statisk boolean for validering
+    let gyldig = true;
 
+    //validering av antall
+    if(antall.trim() === ""){
+        document.getElementById("antallFeil").style.display = "inline";
+        gyldig = false;
+    } else {
+        document.getElementById("antallFeil").style.display = "none";
+    }
+    //validering av fornavn
+    if(fornavn.trim() === ""){
+        document.getElementById("fornavnFeil").style.display = "inline";
+        gyldig = false;
+    } else{
+        document.getElementById("fornavnFeil").style.display = "none";
+    }
+    //validering av etternavn
+    if(etternavn.trim() === ""){
+        document.getElementById("etternavnFeil").style.display = "inline";
+        gyldig = false;
+    } else{
+        document.getElementById("etternavnFeil").style.display = "none";
+    }
+    //validering av telefonnr
+    if(telefonnr.trim() === ""){
+        document.getElementById("telefonnrFeil").style.display = "inline";
+        gyldig = false;
+    } else{
+        document.getElementById("telefonnrFeil").style.display = "none";
+    }
+    //validering av epost
+    if(epost.trim() === ""){
+        document.getElementById("epostFeil").style.display = "inline";
+        gyldig = false;
+    } else{
+        document.getElementById("epostFeil").style.display = "none";
+    }
+
+    if(!gyldig) return;
+
+    biletter.push({film,antall,fornavn,etternavn,telefonnr,epost});
 }
