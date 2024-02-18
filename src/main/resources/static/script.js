01397afc22a523433d858b2c37f16a7da658ef69
mrpythen <herman.nysted@gmail.com>
2024-02-18 16:50:04 +0100

    Laget variabler for input verdiene og lagt inn feilmeldinger i html

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,11 @@
+let biletter = [];
+function bestillBiletter(){
+    let filmer = document.getElementById("fimer").value;
+    let antall = document.getElementById("antall").value;
+    let fornavn = document.getElementById("fornavn").value;
+    let etternavn = document.getElementById("etternavn").value;
+    let telefonnr = document.getElementById("telefonnr").value;
+    let epost = document.getElementById("epost").value;
+
+
+}
